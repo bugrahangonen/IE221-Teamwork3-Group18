8b6fa276fc383e3001aabf04fe6f236cfd954f5f
BugrahanGonen <bugrahan.gonen@agu.edu.tr>
2025-12-30 21:35:23 +0300

    Add SLLN skeleton file

---
+++ src/simulation/SLLNSimulation.java
@@ -0,0 +1,14 @@
+package simulation;
+
+/*
+ * TODO:
+ * This file will be implemented by Person B.
+ * Task: Strong Law of Large Numbers (SLLN)
+ * Output: results/figures/slln.png
+ */
+
+public class SLLNSimulation {
+    public static void main(String[] args) {
+        // implementation will be added by Person B
+    }
+}
