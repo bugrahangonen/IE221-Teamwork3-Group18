2d8937a8d512629af2b1bbb770ed4125aa12221a
BugrahanGonen <bugrahan.gonen@agu.edu.tr>
2025-12-30 21:44:50 +0300

    Add CLT skeleton file

---
+++ src/simulation/CLTSimulation.java
@@ -0,0 +1,14 @@
+package simulation;
+
+/*
+ * TODO:
+ * Implemented by Person D
+ * Task: Central Limit Theorem (CLT)
+ * Output: results/figures/clt.png
+ */
+
+public class CLTSimulation {
+    public static void main(String[] args) {
+        // implementation will be added
+    }
+}
