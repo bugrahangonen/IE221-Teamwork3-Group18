6a35f2ca3ea358bf33612f87f3ff2f9ae7e03318
BugrahanGonen <bugrahan.gonen@agu.edu.tr>
2025-12-30 21:42:54 +0300

    Add Monte Carlo Pi skeleton file

---
+++ src/simulation/MonteCarloPi.java
@@ -0,0 +1,14 @@
+package simulation;
+
+/*
+ * TODO:
+ * Implemented by Person C
+ * Task: Monte Carlo estimation of Pi
+ * Output: results/figures/pi.png
+ */
+
+public class MonteCarloPi {
+    public static void main(String[] args) {
+        // implementation will be added
+    }
+}
